9557d28eaa84067e837c8a07c49c263e7c66befa
sylfleur <cfleurian@sylfleur.com>
2022-01-09 05:11:38 +0100

    add task to array & put in DOM

--- index.js
+++ index.js
@@ -8,23 +8,10 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     data = form.elements[0].value;
     tasks.push(data);
-    loadTable(data);
-    console.log(tasks);
-    //tBody.innerHTML += addTask(data);
+    putDataInTable(data);
 });
 
-// function addTask(data) {
-//     return `<tr>
-//     <th scope="row"><input class="form-check-input mt-0" type="checkbox" value=""></th>
-//     <td>${data}</td>
-//     <td>
-//         <button type="button" class="btn btn-warning">Modifier</button>
-//         <button type="button" class="btn btn-danger">Supprimer</button>
-//     </td>
-//     </tr>`
-// };
-
-const loadTable = (data) => {
+const putDataInTable = (data) => {
     // for (data in tasks) {
     tBody.innerHTML += `<tr>
         <th scope="row"><input class="form-check-input mt-0" type="checkbox" value=""></th>
